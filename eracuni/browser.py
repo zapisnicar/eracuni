ff3d110bb2a42bc0ec1eeadfed1fb4fc89a90803
zapisnicar <8794916+zapisnicar@users.noreply.github.com>
2021-01-22 18:55:47 +0100

    Add implicit wait for selenium browser

--- eracuni/browser.py
+++ eracuni/browser.py
@@ -31,8 +31,9 @@ def firefox(config):
                               'application/octet-stream, application/pdf, application/x-www-form-urlencoded')
     my_profile.set_preference('browser.helperApps.neverAsk.saveToDisk',
                               'application/octet-stream, application/pdf, application/x-www-form-urlencoded')
-
-    return webdriver.Firefox(executable_path=config.gecko_path, options=my_options, firefox_profile=my_profile)
+    driver = webdriver.Firefox(executable_path=config.gecko_path, options=my_options, firefox_profile=my_profile)
+    driver.implicitly_wait(20)
+    return driver
 
 
 def find_first_id(browser, target):
