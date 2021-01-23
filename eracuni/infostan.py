6b016cb5a64afcc24f3686d44d32fe5c26dff1ba
zapisnicar <8794916+zapisnicar@users.noreply.github.com>
2021-01-23 22:31:25 +0100

    Add logout from InfoStan

--- eracuni/infostan.py
+++ eracuni/infostan.py
@@ -40,38 +40,46 @@ class Infostan:
             icon_infostan = find_first_id(self.driver, '1_ЈКП Инфостан Технологије')
             icon_infostan.click()
 
-            # Find all locations
+            # Find number of locations and iterate through them, as i -> (1..n)
             number_of_locations = len(find_all_css(self.driver, 'div.row-item'))
             for i in range(1, number_of_locations + 1):
-                # Choose location
-                row = find_first_css(self.driver,
-                                     f'.container-page > div:nth-child(4) > div:nth-child({i}) > div:nth-child(1)')
-                row.click()
+                # Choose location, i-th row in location table
+                row_i = find_first_css(
+                    self.driver, f'.container-page > div:nth-child(4) > div:nth-child({i}) > div:nth-child(1)')
+                row_i.click()
                 # Find top row, with last bill
                 last_row = find_first_css(self.driver, 'div.row-item')
-                last_period_cell = find_first_css(last_row, '.rowItemName > a')
-                print(last_period_cell.get_attribute('text'))
-                # Click on top row, for side menu
+                last_bill_date = find_first_css(last_row, '.rowItemName > a').get_attribute('text')
+                print(last_bill_date)
+                # Click on top row, for right side menu
                 last_row.click()
                 # Find "Pregled računa" button
                 pregled_racuna_button = find_first_id(driver, 'step5')
                 pregled_racuna_button.click()
+
                 # Wait until page load
                 WebDriverWait(driver, self.config.timeout).until(
                     expected_conditions.presence_of_element_located((By.CSS_SELECTOR, 'div.page[data-loaded="true"')))
                 # Find Download icon
                 save_button = find_first_id(driver, 'download')
                 save_button.click()
-                # Wait 1 sec
+
                 time.sleep(1)
+
                 # Find (X) - Close button
                 close_button = find_first_css(driver, 'div.pdfCloseBtn>span.close-btn')
                 close_button.click()
                 # Find back button
                 back_button = find_first_css(driver, 'div.icon-back')
                 back_button.click()
+                # Wait 1 sec
+                time.sleep(1)
+
+            # Logout
+            logout_button = find_first_css(driver, 'div.mainBtnsHolder>div:nth-child(7)')
+            logout_button.click()
+            confirm_button = find_first_css(driver, 'div.modal-delete-action > button:nth-child(1)')
+            confirm_button.click()
 
             print('End of program')
-            time.sleep(10)
-
-
+            time.sleep(3)
